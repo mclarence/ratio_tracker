096b1875887a0368eff1b002d1f155e915614c3c
Clarence Mesina <mesinaclarence464@gmail.com>
2020-10-13 09:09:20 +1100

    fix time

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ from Utils import get_tracker
 from influxdb_client import InfluxDBClient, Point
 from influxdb_client.client.write_api import SYNCHRONOUS
 import logging
+from datetime import timezone
 from datetime import datetime
-
 logging.basicConfig(format='%(asctime)s [%(levelname)8s] - %(message)s', datefmt='%d-%b-%y %H:%M:%S',
                     level=logging.DEBUG)
 
@@ -18,7 +18,7 @@ influxdb_pass = config['InfluxDB']['Password']
 client = InfluxDBClient(url=config['InfluxDB']['URL'], token=f'{influxdb_user}:{influxdb_pass}', org='-')
 write_api = client.write_api(write_options=SYNCHRONOUS)
 
-time_now = datetime.now()
+time_now = datetime.now(tz=timezone.utc)
 dt_string = time_now.strftime("%d/%m/%Y %H:%M:%S")
 logging.info("The current date and time is " + dt_string)
 
@@ -36,7 +36,7 @@ for section in config.sections():
 
             logging.info(f"({trackerName}) Ratio: {stats.ratio} Downloaded: {stats.download} ({humanfriendly.format_size(stats.download, binary=True)}) Uploaded: {stats.upload} ({humanfriendly.format_size(stats.upload, binary=True)})")
 
-            p = Point("ratio").time(time_now).tag("tracker", trackerName).field("ratio", stats.ratio).field("download", stats.download).field("upload", stats.upload)
+            p = Point("ratio").time(time_now).time(time_now).tag("tracker", trackerName).field("ratio", stats.ratio).field("download", stats.download).field("upload", stats.upload)
 
             try:
                 write_api.write(bucket=config['InfluxDB']['Database'], org='-' ,record=p)
